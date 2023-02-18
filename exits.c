ed9c85b6256467fc6bf66fae409e6029df0b4645
jannat <jma24934@gmail.com>
2023-02-18 10:49:56 -0800

    Partner project.

--- exits.c
+++ exits.c
@@ -58,9 +58,9 @@ char *_strncat(char *dest, char *src, int n)
 }
 
 /**
- * *_strchr - locates a character in a string
+ * *_strchr -- locates a character in a string
  * @s: the string to be parsed
- * @c: the character to look for
+ * @c: the character to be looked for
  * Return: (s) a pointer to the memory area s
  */
 char *_strchr(char *s, char c)
