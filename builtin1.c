3f55a029d00377a9bb3a90872047096e65491ceb
jannat <jma24934@gmail.com>
2023-02-21 12:21:16 -0800

    Updated

--- builtin1.c
+++ builtin1.c
@@ -50,7 +50,7 @@ int set_alias(info_t *info, char *str)
 	p = _strchr(str, '=');
 	if (!p)
 		return (1);
-	if (!*++preturn)
+	if (!*sigreturn)
 		(unset_alias(info, str));
 	unset_alias(info, str);
 	return (add_node_end(&(info->alias), str, 0) == NULL);
